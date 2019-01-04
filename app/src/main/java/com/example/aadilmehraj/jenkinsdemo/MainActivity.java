d6e26aadfe18401ebbd91c2e8eb0515a23987344
Aadil Mehraj <aadilmehraj76@gmail.com>
2019-01-04 11:12:38 +0530

    Add toast message

--- app/src/main/java/com/example/aadilmehraj/jenkinsdemo/MainActivity.java
+++ app/src/main/java/com/example/aadilmehraj/jenkinsdemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.aadilmehraj.jenkinsdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void showToast(View view) {
+        Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show();
     }
 }
